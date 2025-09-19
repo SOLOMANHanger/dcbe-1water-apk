b93bbc5da89e8d33319ce0a3a357caed7ebba3e8
SolomanHanger <kytes2ritez@outlook.com>
2025-09-18 21:30:41 -0500

    Update index.jsx/took out code that did not match thr current template

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,6 @@
-// @ts-nocheck
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { createRoot } from "react-dom/client";
 
-const root = createRoot(document.getElementById('root'));
-root.render(<App/>)
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
 
